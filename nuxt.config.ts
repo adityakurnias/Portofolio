32665dc4e9d266b9c5c70098d0a73fb2e9e90e4f
adityakurnias <adityakurniasaputra903@gmail.com>
2026-01-10 11:53:01 +0700

    feat: homepage with animatin and navigation bar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,16 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
+
   vite: { 
     plugins: [
       tailwindcss()
     ] 
   },
+
+  modules: ["@tresjs/nuxt"],
+  tres: {
+      devtools: true,
+      glsl: true,
+    },
 });
