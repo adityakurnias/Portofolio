94238eec1bdab226fdd06eeaaf01fb861c14e849
adityakurnias <adityakurniasaputra903@gmail.com>
2026-07-27 20:23:58 +0700

    chore(deps): update depedencies to latest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  css: ["./app/assets/css/main.css"],
+  css: ["~/assets/css/main.css"],
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
